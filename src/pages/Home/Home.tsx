aad19d8e928df6fc99f6a254d54879f15bd24313
faridasad <asadlifaridd@gmail.com>
2023-03-04 14:13:59 +0400

    Retrieve all posts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import "./home.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+interface PostProps {
+  image: string;
+  name: string;
+  prompt: string;
 }
 
-export default Home
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(import.meta.env.VITE_SERVER_URL);
+      setData(res.data.data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    data && (
+      <div className="home-grid">
+        {data.map((post: PostProps, i) => {
+          return (
+            <Card
+              image={post.image}
+              name={post.name}
+              prompt={post.prompt}
+              key={i}
+              type={
+                i % 2 === 0
+                  ? "h-strecth"
+                  : i % 3 === 0 && i % 2 === 0
+                  ? "v-strecth"
+                  : "big-strecth"
+              }
+            />
+          );
+        })}
+      </div>
+    )
+  );
+};
+
+export default Home;
