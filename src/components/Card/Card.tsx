c462ad855ffd34d1857432eb25378e58e656994c
faridasad <asadlifaridd@gmail.com>
2023-03-06 23:12:51 +0400

    Add extra card features

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,6 @@
 import "./card.scss";
+import shareIcon from "../../assets/images/share-outline.svg";
+import downloadIcon from "../../assets/images/download-outline.svg";
 
 interface CardProps {
   image: string;
@@ -8,14 +10,27 @@ interface CardProps {
 }
 
 const Card = ({ image, name, prompt, type }: CardProps) => {
+
   return (
-    <div className={`card ${type}`}>
+    <div className={`card ${type}`} style={{["--user-name" as any]: name}}>
       <img src={image} />
       <div className="top">
         <p>{name}</p>
       </div>
       <div className="bottom">
-        <p>{prompt}</p>
+        <div className="left">
+          <span className="user-p" data-name={name}>
+            <p>{name.split("")[0].toUpperCase()}</p>
+          </span>
+        </div>
+        <div className="right">
+          <span className="share">
+            <img src={shareIcon} />
+          </span>
+          <span className="download">
+            <img src={downloadIcon} />
+          </span>
+        </div>
       </div>
     </div>
   );
