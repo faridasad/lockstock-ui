cfb56988be62ca7d17cb86299b7cea4fddc67845
faridasad <asadlifaridd@gmail.com>
2023-03-12 22:49:27 +0400

    Add individual post

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,21 @@
 import "./home.scss";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "../../components/Card/Card";
+import { useLocation, useNavigate } from "react-router-dom";
+import Modal from "../../components/Modal/Modal";
 
 interface PostProps {
   image: string;
   name: string;
   prompt: string;
+  _id: string;
 }
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [singlePost, setSinglePost] = useState<any>({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -20,27 +25,59 @@ const Home = () => {
     fetchPosts();
   }, []);
 
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      const fetchSinglePost = async () => {
+        const res = await axios.get(
+          `${import.meta.env.VITE_SERVER_URL}/${location.pathname.slice(1)}`
+        );
+
+        setSinglePost(res.data.data);
+      };
+      fetchSinglePost();
+      document.body.style.overflow = "hidden";
+    }
+  }, [location]);
+
+  function onClose() {
+    navigate("/");
+    setSinglePost({});
+    document.body.style.overflow = "auto";
+  }
+
   return (
     data && (
-      <div className="home-grid">
-        {data.map((post: PostProps, i) => {
-          return (
-            <Card
-              image={post.image}
-              name={post.name}
-              prompt={post.prompt}
-              key={i}
-              type={
-                i % 3 === 0
-                  ? "h-strecth"
-                  : i % 3 === 0 && i % 2 === 0
-                  ? "v-strecth"
-                  : "big-strecth"
-              }
-            />
-          );
-        })}
-      </div>
+      <>
+        <div className="home-grid">
+          {data.map((post: PostProps, i) => {
+            return (
+              <Card
+                image={post.image}
+                name={post.name}
+                prompt={post.prompt}
+                key={i}
+                id={post._id}
+                type={
+                  i % 3 === 0
+                    ? "h-strecth"
+                    : i % 3 === 0 && i % 2 === 0
+                    ? "v-strecth"
+                    : "big-strecth"
+                }
+              />
+            );
+          })}
+        </div>
+        <Modal isOpen={Object.keys(singlePost).length !== 0} onClose={onClose}>
+          <div className="modal-content">
+            <img src={singlePost.image} />
+            <p>{singlePost.prompt}</p>
+            <p>- {singlePost.name}</p>
+          </div>
+        </Modal>
+      </>
     )
   );
 };
