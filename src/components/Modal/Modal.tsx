aaf9f3ea94120d40a672d171a13395efbced0241
faridasad <asadlifaridd@gmail.com>
2023-04-12 14:38:46 +0400

    Close when clicking outside

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,21 @@
 import ReactDom from "react-dom";
+import { useRef } from "react";
 import "./modal.scss";
 
 const Modal = ({ children, isOpen, onClose }: any) => {
   if (!isOpen) return null;
 
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
   return ReactDom.createPortal(
-    <div className="overlay">
-      <div className="modal">
+    <div className="overlay" onClick={handleClick}>
+      <div className="modal" ref={modalRef}>
         <span className="close-btn" onClick={onClose}>X</span>
         {children}
       </div>
